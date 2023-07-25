c0da60d76b4134bb9096b1496f7350453bf4257d
Amin Soltani <asoltani927@gmail.com>
2023-07-25 22:20:27 +0330

    (feat/web-client) create route and middleware for web NOT #3

---
+++ packages/web-client/src/layouts/DefaultLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  <div class="container-fluid">
+    <div id="layout-wrapper">
+      <RouterView v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </RouterView>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
